790c5ab8312e3dbece9caee064b40b8569d88ad5
sji8562 <whaqlzkem@naver.com>
2024-01-08 15:16:40 +0900

    [송재익] 관리자 유ì  gi

--- src/main/java/com/tenco/projectinit/controller/mng/MngUserController.java
+++ src/main/java/com/tenco/projectinit/controller/mng/MngUserController.java
@@ -1,12 +1,25 @@
 package com.tenco.projectinit.controller.mng;
 
+import com.tenco.projectinit.repository.entity.User;
+import com.tenco.projectinit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/mng/user")
 public class MngUserController {
-    public String userList(){
-        return "/mng/list";
+
+    @Autowired
+    UserService userService;
+    @GetMapping("list")
+    public String userList(Model model){
+        List<User> userList = userService.findAll();
+        model.addAttribute("userList",userList);
+        return "/mng/user/list";
     }
 }
