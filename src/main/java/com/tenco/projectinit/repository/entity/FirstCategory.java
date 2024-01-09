7a23484edd48d44fda61b09a576752a8446f1be2
sji8562 <whaqlzkem@naver.com>
2024-01-09 15:30:47 +0900

    관리자 퍼스트카테고리 리스트 애드 딜리트

--- src/main/java/com/tenco/projectinit/repository/entity/FirstCategory.java
+++ src/main/java/com/tenco/projectinit/repository/entity/FirstCategory.java
@@ -17,7 +17,6 @@ public class FirstCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String name;
 
 
--- src/main/java/com/tenco/projectinit/repository/inteface/FirstCategoryJPARepository.java
+++ src/main/java/com/tenco/projectinit/repository/inteface/FirstCategoryJPARepository.java
@@ -1,7 +1,20 @@
 package com.tenco.projectinit.repository.inteface;
 
 import com.tenco.projectinit.repository.entity.FirstCategory;
+import jakarta.transaction.Transactional;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface FirstCategoryJPARepository extends JpaRepository<FirstCategory, Integer> {
+    FirstCategory findByName(String fCategoryName);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO first_category_tb (name) VALUES (:name)", nativeQuery = true)
+    int saveByName(String name);
 }
