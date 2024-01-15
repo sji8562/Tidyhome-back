34e72c44b2cadca3e3d968413a5a44fc0649f555
Jys <onlys2545@naver.com>
2024-01-15 16:12:34 +0900

    재수정

--- src/main/java/com/tenco/projectinit/dto/responsedto/RequestResponseDTO.java
+++ src/main/java/com/tenco/projectinit/dto/responsedto/RequestResponseDTO.java
@@ -6,10 +6,14 @@ import lombok.Data;
 public class RequestResponseDTO {
     @Data
     public static class RequestDTO{
+        private Integer requestId;
+        private Integer etcInfo;
         private String special;
         private String otherRequest;
 
-        public RequestDTO(String special, String otherRequest) {
+        public RequestDTO(Integer requestId, Integer etcInfo, String special, String otherRequest) {
+            this.requestId = requestId;
+            this.etcInfo = etcInfo;
             this.special = special;
             this.otherRequest = otherRequest;
         }
