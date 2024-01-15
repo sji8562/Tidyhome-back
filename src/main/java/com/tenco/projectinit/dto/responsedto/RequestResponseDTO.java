ed7ac9ff5ec3fce2fadd4f98601047c867f37ac3
DJ0H <odajung02@gmail.com>
2024-01-15 10:48:56 +0900

    [DJ0H] í관리자 결제취소

--- src/main/java/com/tenco/projectinit/dto/responsedto/RequestResponseDTO.java
+++ src/main/java/com/tenco/projectinit/dto/responsedto/RequestResponseDTO.java
@@ -5,16 +5,13 @@ import lombok.Data;
 public class RequestResponseDTO {
     @Data
     public static class RequestDTO{
-        private Integer requestId;
-        private Integer etcInfo;
         private String special;
         private String otherRequest;
 
-        public RequestDTO(Integer requestId, Integer etcInfo, String special, String otherRequest) {
-            this.requestId = requestId;
-            this.etcInfo = etcInfo;
+        public RequestDTO(String special, String otherRequest) {
             this.special = special;
             this.otherRequest = otherRequest;
         }
     }
+
 }
---
+++ src/main/java/com/tenco/projectinit/repository/inteface/WaitJPARepository.java
@@ -0,0 +1,10 @@
+package com.tenco.projectinit.repository.inteface;
+
+import com.tenco.projectinit.repository.entity.sub_entity.Wait;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WaitJPARepository extends JpaRepository<Wait, Integer> {
+    Optional<Wait> findByReservationSuc_Reservation_Id(Integer reservationId);
+}
