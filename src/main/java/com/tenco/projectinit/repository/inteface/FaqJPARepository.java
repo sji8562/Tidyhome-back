276e7d2ae0249b02223ad8f8c947d1fd4a8ed1d2
sji8562 <whaqlzkem@naver.com>
2024-01-17 10:50:45 +0900

    쿼리문 교체

--- src/main/java/com/tenco/projectinit/repository/inteface/FaqJPARepository.java
+++ src/main/java/com/tenco/projectinit/repository/inteface/FaqJPARepository.java
@@ -3,6 +3,7 @@ package com.tenco.projectinit.repository.inteface;
 import com.tenco.projectinit.dto.mng.FaqResponseDTO;
 import com.tenco.projectinit.dto.responsedto.FaqPartResponseDTO;
 import com.tenco.projectinit.repository.entity.Faq;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,9 +13,8 @@ public interface FaqJPARepository extends JpaRepository<Faq, Integer> {
     @Query(value = "select fp.id as id,f.name as name,fp.title as title ,fp.code as code from Faq f left join FaqPart fp on f.id = fp.faq.id")
     List<FaqResponseDTO.faqListDTO> findAllWithFaqPart();
 
-    @Query(value = "select f.id as faqId,f.name as name,fp.id as partId,fp.title as title,fp.content as content,fp.code as code from Faq f left join FaqPart fp where f.service.id = :id and fp.faq.id = 1")
-    List<FaqPartResponseDTO.FaqJoinPartDTO> findAllAndServicesId(Integer id);
-
-    @Query(value = "select f.id as faqId,f.name as name,fp.id as partId,fp.title as title,fp.content as content,fp.code as code from Faq f left join FaqPart fp where f.service.id = :id and fp.faq.id = :partId")
-    List<FaqPartResponseDTO.FaqJoinPartDTO> findAllAndServicesIdAndPartId(Integer id,Integer partId);
+    @Query(value = "select new com.tenco.projectinit.dto.responsedto.FaqPartResponseDTO$FaqJoinPartDTO(ft.id as faqId, ft.name as name,fpt.id as partId,fpt.title as title,fpt.content as content,fpt.code as code) " +
+            "from Faq ft left join FaqPart fpt on ft.id = fpt.faq.id" +
+            " where ft.service.id = :id and ft.id = :partId")
+    List<FaqPartResponseDTO.FaqJoinPartDTO> findAllAndServicesIdAndPartId(@Param("id") Integer id, @Param("partId") Integer partId);
 }
